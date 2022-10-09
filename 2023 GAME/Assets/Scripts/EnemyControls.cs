2b7dc707ef9aa8f0100a26e4bad99dd2afc9e5fa
Shawn Hope <shawnelmohope@gmail.com>
2022-10-09 15:55:32 -0600

    Setup (Enemy) Animator and Enemy Movement
    - Enemies now use the same placeholder animations as the player's
    - Enemies roll a number 1-10 to decide whether or not to move to the player's position or roll again
    
    Still needs more brainstorming..

--- 2023 GAME/Assets/Scripts/EnemyControls.cs
+++ 2023 GAME/Assets/Scripts/EnemyControls.cs
@@ -6,17 +6,42 @@ using System;
 public class EnemyControls : MonoBehaviour
 {
     public static Action<GameObject> thisDied;
+
     [SerializeField] int enemyHealth;
     [SerializeField] NavMeshAgent agent;
+    [SerializeField] SpriteRenderer spRen;
+    [SerializeField] GameObject player;
+    [SerializeField] Animator anim;
+    [SerializeField] bool busy;
     private void OnEnable()
     {
         this.agent = this.GetComponent<NavMeshAgent>();
+        this.spRen = this.GetComponent<SpriteRenderer>();
+        this.anim = this.GetComponent<Animator>();
         enemyHealth = 10;
     }
-    void Hit() {
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        if (!busy) { Decide(); }
+
+        FlipCheck();
+        if (agent.remainingDistance > 2.3f) {
+            anim.SetFloat("remainingDis",agent.remainingDistance);
+        }
+    }
+    void Decide() {
+        busy = true;
+        StartCoroutine(WaitToDecide());
+    }
+    void IsHit() {
         print("been hit");
         if (enemyHealth != 1)
         {
+            anim.SetTrigger("staggered");
             enemyHealth--;
         }
         else {
@@ -27,6 +52,34 @@ public class EnemyControls : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        Hit();
+        IsHit();
+    }
+    void FlipCheck() {
+        if (player.transform.position.x < transform.position.x)
+        {
+            spRen.flipX = true;
+        }
+        else {
+            spRen.flipX = false;
+        }
+    }
+    void Chase() {
+        agent.SetDestination(player.transform.position);
+        busy = false;
+    }
+    public int RandomNum() {
+        int num;
+        num = UnityEngine.Random.Range(1,10);
+        print(num);
+        return num;
+    }
+    public IEnumerator WaitToDecide()
+    {
+        yield return new WaitForSeconds(2);
+        if (RandomNum() >= 5)
+        {
+            Chase();
+        }
+        else { busy = false; yield break; }
     }
 }
